eb2e1818f993b16d31f3098fab6bfccbc23556f8
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2019-09-12 18:36:07 +0800

    Fixed manifest. Modified meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './styles/index.scss';
 
-import App from './scripts/main';
+import App from './scripts/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
