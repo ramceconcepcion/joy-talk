8ef8b65502cc2e622d9d697641e194369282e52d
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2019-09-13 14:18:09 +0800

    Fixed form submit behavior on login

--- src/scripts/components/Login.tsx
+++ src/scripts/components/Login.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 class Login extends React.Component<any, any>{
     constructor(props) {
         super(props);
+
+        this.submit = this.submit.bind(this);
+    }
+
+    submit(e) {
+        e.preventDefault();
+        this.props.onSubmit(this.refs.input1);
     }
 
     render() {
@@ -10,7 +17,7 @@ class Login extends React.Component<any, any>{
             <div className="login">
                 <div className="login-title">JoyTalk</div>
                 <div className="login-subtitle">Your quick chat solution against workplace IT.</div>
-                <form className="login-form" onSubmit={(e) => this.props.onSubmit(this.refs.input1)}>
+                <form className="login-form" onSubmit={(evt) => this.submit(evt)}>
                     <label htmlFor="">Enter a passcode</label>
                     <input type="text" placeholder="" ref="input1" />
                     <input type="submit" value="Login" />
