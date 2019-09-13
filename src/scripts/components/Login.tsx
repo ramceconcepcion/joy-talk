c27bcef98673bb6ebdbd09943daecc76f808771a
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2019-09-13 12:17:15 +0800

    Added base Login component

---
+++ src/scripts/components/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Users from '../misc/users';
+
+
+class Login extends React.Component<any, any>{
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+
+export default Login;
