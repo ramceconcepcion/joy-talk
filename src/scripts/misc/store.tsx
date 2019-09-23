8960ce289044aaa61e574bd775fd1c0dbcef6a07
Ramce Concepcion <ramuzuconcepcion@gmail.com>
2019-09-23 16:06:44 +0800

    Added base store

--- src/scripts/misc/store.tsx
+++ src/scripts/misc/store.tsx
@@ -1,5 +1,17 @@
 import { createStore } from 'redux';
 
+const state = {
+    //logged in user
+    currentUser: null,
+    currentUserBroadcastTimeoutId: null,
 
+    //all users
+    users: null,
+
+    //chats data
+    chats: [],
+    typing: null,
+    typingTimeoutId: null,
+}
 
 export default {};
